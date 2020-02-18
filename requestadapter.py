c097698eb5048157f20dd03dac1266fe0095f233
Alisa Teploukhova <alisa.teploukhova@aciworldwide.com>
2020-02-18 16:18:58 +0300

    try replacing quotes

--- requestadapter.py
+++ requestadapter.py
@@ -85,7 +85,7 @@ TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
   </Transaction>
 </Request>"""
 
-APP_PARAMS_TEMPLATE = """<Parameter name="OPP_threeDSecure.deviceInfo">{"messageType":"AuthRequest","messageVersion":"${messageVersion}","deviceChannel":"${deviceChannel}","sdkTransID":"${sdkTransID}","sdkAppID":"${sdkAppID}","sdkReferenceNumber":"${sdkReferenceNumber}","sdkEphemPubKey":{\"kty\": \"EC\", \"crv\": \"P-256\", \"x\": \"7L3k5UmR6GoqWoUcEUsZ3zQkRqcSTF4JpF2uaFTbr3Q\", \"y\": \"fOg7r0NSJDjbYRffG1v0Gv5u0oHcfaejirxrM3Cj0bw\"},"sdkEncData":"${sdkEncData}","sdkMaxTimeout":"${sdkMaxTimeout}","sdkInterface":"03","sdkUiType":"31"}</Parameter>"""
+APP_PARAMS_TEMPLATE = """<Parameter name="OPP_threeDSecure.deviceInfo">{"messageType":"AuthRequest","messageVersion":"${messageVersion}","deviceChannel":"${deviceChannel}","sdkTransID":"${sdkTransID}","sdkAppID":"${sdkAppID}","sdkReferenceNumber":"${sdkReferenceNumber}","sdkEphemPubKey":"${SDK_EPHEM_KEY}","sdkEncData":"${sdkEncData}","sdkMaxTimeout":"${sdkMaxTimeout}","sdkInterface":"03","sdkUiType":"31"}</Parameter>"""
 
 BROWSER_PARAMS_TEMPLATE = """<Parameter name="MODEL_customer.browser.acceptHeader">${browserAcceptHeader}</Parameter>
       <Parameter name="MODEL_customer.browser.language">${browserLanguage}</Parameter>
@@ -126,6 +126,8 @@ class RequestAdapter(object):
                     return self.build_template(APP_PARAMS_TEMPLATE)
                 else:
                     return self.build_template(BROWSER_PARAMS_TEMPLATE)
+            elif target == "SDK_EPHEM_KEY":
+                return self.__data["sdkEphemPubKey"].replace("'", '\"')
             else:
                 match = re.fullmatch("([a-z0-9]+)\\(([^}]*)\\)", target)
                 if match is not None:
